d51688c8992e916a5897cd23b9e7972af394b2a2
haodong <haodong.donh@gmail.com>
2024-01-19 18:34:37 +0800

    web响应式编程

--- boot03-01-demo/src/main/java/com/ncamc/boot/MainApplication.java
+++ boot03-01-demo/src/main/java/com/ncamc/boot/MainApplication.java
@@ -6,6 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MainApplication {
     public static void main(String[] args) {
-        SpringApplication.run(MainApplication.class,args);
+        //java10提供的局部变量类型自动推断
+        var ioc= SpringApplication.run(MainApplication.class,args);
+        for (String name : ioc.getBeanDefinitionNames()) {
+            System.out.println(name);
+        }
     }
 }
---
+++ boot03-03-web/src/main/java/com/ncamc/boot03WebApplication.java
@@ -0,0 +1,16 @@
+package com.ncamc;
+
+
+import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.SpringApplicationHook;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+        import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class boot03WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(boot03WebApplication.class, args);
+    }
+
+}
