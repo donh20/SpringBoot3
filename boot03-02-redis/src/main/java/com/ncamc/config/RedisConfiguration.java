7b76da66293eb5f29e4898dd7c4ded43c55cdeb7
Dong Hao <haodong.donh@gmail.com>
2024-03-25 00:20:21 +0800

    修改默认序列化器,定制redistemplate

---
+++ boot03-02-redis/src/main/java/com/ncamc/config/RedisConfiguration.java
@@ -0,0 +1,23 @@
+package com.ncamc.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+@Configuration
+public class RedisConfiguration {
+    /**
+     *
+     * @param redisConnectionFactory
+     * @return
+     */
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        template.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+        return template;
+    }
+}
--- boot03-02-redis/src/main/java/com/ncamc/controller/RedisTestController.java
+++ boot03-02-redis/src/main/java/com/ncamc/controller/RedisTestController.java
@@ -1,6 +1,8 @@
 package com.ncamc.controller;
 
+import com.ncamc.bean.Person;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +12,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class RedisTestController {
 
     @Autowired
-    StringRedisTemplate stringRedisTemplate;
+    StringRedisTemplate StringRedisTemplate;
+
+    @Autowired
+    RedisTemplate redisTemplate;
 
     @GetMapping("/count")
     public String count() {
-        Long hello = stringRedisTemplate.opsForValue().increment("hello");
+        Long hello = StringRedisTemplate.opsForValue().increment("hello");
 
 
         return "访问了["+hello+"]次";
     }
+
+    @GetMapping("/person/save")
+    public String savePerson(){
+        Person person = new Person(1L, "张三", "12345@qq.com", 19, "intern");
+        redisTemplate.opsForValue().set("person",person);
+        return "OK";
+    }
+    @GetMapping("/person/get")
+    public Person getPerson(){
+
+        Person person = (Person) redisTemplate.opsForValue().get("person");
+        return person;
+    }
+
 }
